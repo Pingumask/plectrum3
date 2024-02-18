391dae8c3d309ea298e1cf1515d85e61c5fe184a
Pingumask <Pingumask@elanta.fr>
2024-02-18 21:56:14 +0100

    affichage des erreurs

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require_once './vendor/autoload.php';
 
-use GuzzleHttp\Psr7\Request;
 use Pingumask\Plectrum\App;
+use GuzzleHttp\Psr7\Request;
 
 $client_public_key = App::getConf('discord', 'client_id');
-if (App::getConf('env', 'debug')) {
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
-}
 
 $body = file_get_contents('php://input') ?: '';
 $request = new Request(
